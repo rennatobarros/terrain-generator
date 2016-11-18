b23ed95328046d431f8627e8c11232c0a76b0897
Renato Barros <rennatobarros@gmail.com>
2016-11-18 17:24:28 -0300

    Added some arguments verification

--- main.c
+++ main.c
@@ -4,13 +4,19 @@ int main(int argc, char *argv[])
 {
     //char *parametros[] = {"-d", "-o", "-s"};
     
-    for(i = 0; i < argc; i++) {
-        if(argc > 1){
+    if(argc == 1){
+        printf("Arquivo sem paramentros!!\n");
+    }else if(argc > 1){
+        if(argc % 2 != 0){
             if(strcmp("-d",argv[i + 1]) == 0 || strcmp("-o",argv[i + 1]) == 0){
                 printf("Argumentos aceitos!\n");
             }
-        }
+        }else{
+                printf("Argumentos faltando!\n");
+                exit(0);
+         }
     }
+    
 
             
     
