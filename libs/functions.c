be3ddbdc8bfb63558924b340776982ef61ecce5b
Renato Barros <rennatobarros@gmail.com>
2016-11-29 00:28:28 -0300

    Added new functions

--- libs/functions.c
+++ libs/functions.c
@@ -6,7 +6,7 @@ int num_rand (int max)
     return rand() % max;
 }
 
-void gen_linhacontorno(pixel matriz[MAX+1][MAX+1], ponto inicial, ponto final, ponto vetorLinha[MAX+1], int range)
+void gen_linhacontorno(pixel matriz[MAX][MAX], ponto inicial, ponto final, ponto vetorLinha[MAX], int range)
 {
     if(inicial.coluna < final.coluna && range != 0){
         
@@ -28,7 +28,36 @@ void gen_linhacontorno(pixel matriz[MAX+1][MAX+1], ponto inicial, ponto final, p
     }
 }
 
-void gen_imagem(pixel matriz[MAX+1][MAX+1], ponto vetorLinha[MAX+1])
+void gen_terrain(pixel matriz[MAX][MAX], ponto vetorLinha[MAX])
+{
+    int l = 0;
+
+    for (i = 0; i < MAX; i++){
+        for (j = 0; j < MAX; i++){
+            matriz[i][j] = vetorLinha[l];
+            l++;
+        }
+    }
+}
+
+void salvar_img(pixel matriz[MAX][MAX])
 {
     
+    //char nome_arq_salvar[50];
+    //scanf("%s", nome_arq_salvar);
+
+    foto = fopen(nome_arq_salvar, "w");
+
+    fprintf (foto, "%s\n", header);
+    fprintf (foto, "%d %d\n", height, width);
+    fprintf (foto, "%d\n", v_max);
+
+    for (i=0; i < height; i++)
+        for (j=0; j < width; j++)
+            fprintf(foto, "%d %d %d\n", 
+            //Operação ternária que verifica se o valor do pixel não ultrapassou os valores máximo e mínimo.
+            (matriz[i][j].r > v_max) ? v_max : (matriz[i][j].r < 0) ? 0 : matriz[i][j].r,
+            (matriz[i][j].g > v_max) ? v_max : (matriz[i][j].g < 0) ? 0 : matriz[i][j].g,
+            (matriz[i][j].b > v_max) ? v_max : (matriz[i][j].b < 0) ? 0 : matriz[i][j].b);
+    fclose(fotoN);
 }
