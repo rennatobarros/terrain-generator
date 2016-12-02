c36410d569f4cff66c22804dfb8719eefc2c2cad
Renato Barros <rennatobarros@gmail.com>
2016-12-02 17:14:59 -0300

    Organize the file and add the loop for parameters verification

--- main.c
+++ main.c
@@ -2,45 +2,44 @@
 
 int main(int argc, char *argv[])
 {
-    //char *parametros[] = {"-d", "-o", "-s"};
     srand ((unsigned)time(NULL));
 
-    pixel matriz[MAX][MAX];
-    int size = 128;
+    /** Valor dos limites da linha de contorno. */
+    int size;
 
-    int altitudes[MAX];
+    /** Nome do arquivo a ser salvo. */
+    char *arq[20];
+
+    /** Define o tamanho máximo que a imagem pode ter. */
+    int MAX;
     
-    //for(i = 0; i < size; i++)
-      //  altitudes[i] = 0;
-
-    //for(i = 0; i < size; i++)
-      //  printf("%d ", altitudes[i]);
-    //printf("\n");
-
-    gen_linhacontorno(MAX-1, size, altitudes);
-    gen_terrain(matriz, altitudes);
-
-    //for(i = 0; i < size; i++)
-      //  printf("%d ", altitudes[i]);
-    //printf("\n");
-
-
-
-
-    salvar_img(matriz);
-    
-    /* if(argc == 1){
-        printf("Arquivo sem paramentros!!\n");
-    }else if(argc > 1){
-        if(argc % 2 != 0){
-            if(strcmp("-d",argv[i + 1]) == 0 || strcmp("-o",argv[i + 1]) == 0){
-                printf("Argumentos aceitos!\n");
+    // Loop para capturar e fazer a verificação dos paramêtros passados
+    for(i = 0; i < argc; i++){
+        if(argc == 1){
+            //Valores default
+            size = 128, arq = "terrain", MAX = 513;
+        // Verifica se tem argumentos seguido de parâmetros
+        }else if(argc > 1 && argc % 2 == 1){
+            // Verifica se a opção digitada confere com a já estabelicida e atribui o argumento passado a corresponde variável
+            if(strcmp("-d",argv[i + 1]) == 0){
+                size = atoi(argv[i + 1]);
+            }else if(strcmp("-o",argv[i + 1]) == 0){
+                arq = argv[i + 1];
+            }else if(strcmp("-s",argv[i + 1]) == 0){
+                MAX = atoi(argv[i + 1]);
             }
         }else{
-                printf("Argumentos faltando!\n");
-                exit(0);
-         }
-    } */
+            printf("Argumentos faltando!\n");
+            exit(0);
+        }
+    }
+
+    pixel matriz[MAX][MAX];
+    int altitudes[MAX];
+    
+    gen_linhacontorno(MAX-1, size, altitudes);
+    gen_terrain(matriz, altitudes);
+    salvar_img(matriz, arq);
             
     
     return 0;
