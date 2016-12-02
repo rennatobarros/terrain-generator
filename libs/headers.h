d5f670092374d81bb2fd24a77d2baeb84f8833b8
Renato Barros <rennatobarros@gmail.com>
2016-12-02 17:11:52 -0300

    Change the header of 'salvar_img'

--- libs/headers.h
+++ libs/headers.h
@@ -5,7 +5,7 @@
 
 int num_rand (int);
 void gen_linhacontorno(int size, int limit, int altitudes[size]);
-void salvar_img(pixel matriz[MAX][MAX]);
+void salvar_img(pixel matriz[MAX][MAX], char file[]);
 void gen_terrain(pixel matriz[MAX][MAX], int altitudes[size]);
 
 #endif
