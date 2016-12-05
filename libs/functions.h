ed95019592ae20c3ddb6f81b93c10fb20ccd58f3
Renato Barros <rennatobarros@gmail.com>
2016-12-05 19:58:58 -0300

    Move some functions to 'manip_matriz'

--- libs/functions.h
+++ libs/functions.h
@@ -1,35 +1,25 @@
 #include "headers.h"
 #include "global.h"
+#include "manip_matriz.h"
 
 int num_rand (int max) 
 {
     return rand()%max;
 }
 
-void gen_terrain(pixel matriz[MAX][MAX], int altitudes[MAX])
-{
-    int aux;
-    
-    for (j=0; j < MAX; j++){
-		aux = altitudes[j];
-		for (i=aux; i < MAX; i++){
-			matriz[i][j].r = 40, // Seta a cor marrom para indicar uma montanha.
-            matriz[i][j].g = 20,
-            matriz[i][j].b = 13;
-			
-		}
-	}
-}
-
 void salvar_img(int MAX, pixel matriz[MAX][MAX], char file[])
 {
+    // Define que cada pixel só poderá ir até 255
     v_max = 255;
 
     // Concatena o nome do arquivo com a extensão ppm
     foto = fopen(strcat(file,".ppm"), "w");
 
+    // Define o cabeçalho da imagem 
     fprintf (foto, "%s\n", header);
+    // Define os valor de altura e largura da imagem
     fprintf (foto, "%d %d\n", MAX, MAX);
+    // Define o valor máximo que cada pixel pode alcançar
     fprintf (foto, "%d\n", v_max);
 
     for (i=0; i < MAX; i++)
@@ -40,53 +30,6 @@ void salvar_img(int MAX, pixel matriz[MAX][MAX], char file[])
             (matriz[i][j].g > v_max) ? v_max : (matriz[i][j].g < 0) ? 0 : matriz[i][j].g,
             (matriz[i][j].b > v_max) ? v_max : (matriz[i][j].b < 0) ? 0 : matriz[i][j].b);
     fclose(foto);
-}
-
-void midPoint(int size, int altitudes[MAX], int limit, int inicial, int final)
-{
-    
-    if (inicial >= 0 && final < size)
-    {
-        int displacement;
-        if(limit >= 2){
-            displacement = num_rand(limit);
-        }else{
-            displacement = 0;
-        }
-
-        int midpoint = (final-inicial)/2;
-
-        if(midpoint > 0){
-            int temp = ((altitudes[inicial] + altitudes[final])/2 + displacement);
-                
-            altitudes[inicial+midpoint] = temp;
-
-            if(temp > size-1)
-                altitudes[inicial+midpoint] = size-1;
-            else if(temp < 0)
-               altitudes[inicial+midpoint] = 0;
-
-        
-            //int num = num_rand(limit);
-            
-        
-            // Preenche do inicio até o ponto médio
-            midPoint(size, altitudes, limit/2, inicial, midpoint+inicial);
-            // Preenche do ponto médio até o final
-            midPoint(size, altitudes, limit/2, midpoint+inicial, final);
-        }
-            
-    }
-}
-
-void gen_linhacontorno(int size, int limit, int altitudes[MAX])
-{
-    // Setando aleatoriamente os pontos inicial e final, respectivamente.
-    altitudes[0] = num_rand(size);
-    altitudes[size-1] = num_rand(size);
-
-    // Chama a função para gerar os pontos médios.
-    midPoint(size, altitudes, limit, 0, size-1);
 }
 
 void init_prog(int MAX, int size, char arq[])
