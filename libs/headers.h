a16e3dcc293ca58b30c38f253731dbb4e580deb5
Renato Barros <rennatobarros@gmail.com>
2016-12-07 14:06:29 -0300

    Remove unnecessary include

--- libs/headers.h
+++ libs/headers.h
@@ -1,8 +1,6 @@
 #ifndef __HEADERS_H
 #define __HEADERS_H
 
-#include "global.h"
-
 int 	num_rand 			(int);
 void 	init_prog			(int MAX, int deslocamento, char arq[], int p_dia);
 void 	gen_linhacontorno	(int MAX, int deslocamento, int tipo, int montanha[MAX], pixel matriz[MAX][MAX]);
