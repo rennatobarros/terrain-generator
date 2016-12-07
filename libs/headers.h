32e9528ac73c80488fe990cf2c4ddb0f735f7564
Renato Barros <rennatobarros@gmail.com>
2016-12-07 14:28:47 -0300

    Added Doxygen's comments

--- libs/headers.h
+++ libs/headers.h
@@ -1,11 +1,21 @@
+/** 
+ * \file headers.h
+ * \brief Arquivo com os cabeçalhos das funções
+ *
+ * Arquivo com os cabeçalhos de cada função afim de evitar erros de declaração implicita
+ *
+ */
+
 #ifndef __HEADERS_H
 #define __HEADERS_H
 
+// functions.h
 int 	num_rand 			(int);
 void 	init_prog			(int MAX, int deslocamento, char arq[], int p_dia);
-void 	gen_linhacontorno	(int MAX, int deslocamento, int tipo, int montanha[MAX], pixel matriz[MAX][MAX]);
-void    midPoint            (int MAX, int montanha[MAX], pixel matriz[MAX][MAX], int tipo, int deslocamento, int inicial, int final);
 void 	salvar_img			(int MAX, pixel matriz[MAX][MAX], char file[]);
 
+// manip_matriz.h
+void    midPoint            (int MAX, int montanha[MAX], pixel matriz[MAX][MAX], int tipo, int deslocamento, int inicial, int final);
+void 	gen_linhacontorno	(int MAX, int deslocamento, int tipo, int montanha[MAX], pixel matriz[MAX][MAX]);
 
 #endif
